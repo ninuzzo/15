359451dd7eade1db49df065b80f8c3b8bc084688
Antonio Bonifati <antonio.bonifati@gmail.com>
2014-09-07 01:40:12 +0200

    non stampa NULL e codice namespaziato

--- gioco_del_15.js
+++ gioco_del_15.js
@@ -1,18 +1,23 @@
 // Stampa la tabellina.
 
-// Ordine iniziale.
-var tabellina = [
-  [3, 1, 6, 2],
-  [5, 7, 15, 13],
-  [4, 11, 8, 9],
-  [14, 10, 12, null]
-];
+(function() {
+  // Ordine iniziale.
+  var tabellina = [
+    [3, 1, 6, 2],
+    [5, 7, 15, 13],
+    [4, 11, 8, 9],
+    [14, 10, 12, null]
+  ], riga, colonna, numero;
 
-document.write('<table>');
-for (riga = 0; riga < 4; riga++) {
-  document.write('<tr>');
-  for (colonna = 0; colonna < 4; colonna++) {
-    document.write('<td><button>' + tabellina[riga][colonna] + '</button>');
+  document.write('<table>');
+  for (riga = 0; riga < 4; riga++) {
+    document.write('<tr>');
+    for (colonna = 0; colonna < 4; colonna++) {
+      document.write('<td>');
+      if (numero = tabellina[riga][colonna]) {
+        document.write('<button>' + numero + '</button>');
+      }
+    }
   }
-}
-document.write('</table>');
+  document.write('</table>');
+}());
